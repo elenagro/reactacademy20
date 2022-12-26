4ea8360ced03f66c6ab8ddb44b3334ec0e1e86f0
elenagro <ele.vasevska@gmail.com>
2022-12-26 19:58:01 +0100

    main clean up

--- src/App.js
+++ src/App.js
@@ -1,50 +1,6 @@
-import Student from "./components/Student";
-import PersonIterator from './components/PersonIterator'
-import StudentClass from './components/StudentClass'
-
-import './css/style.css'
-
-// const name = "Mia Koluci"
-// const age = 31
-// const toShow = true
-// const fruits = ['apple', 'android', 'banana']
-// const student = {
-//   name,
-//   age,
-//   fruits
-// }
-
 const App = () => {
-const name = "Mia Koluci"
-const age = 31
-const toShow = true
-const fruits = ['apple', 'android', 'banana']
-const student = {
-  name,
-  age,
-  fruits
+
+  return (<></>);
 }
-  return (<>
-    <div style={{fontSize: '10px'}}>Hello World</div>
-    <Student fruits={fruits} name={name} age={age} shouldTheComponentsShow={toShow} />
-
-    <Student fruits={['orange', 'lemon', 'lime']} name={'Svetle'} age={44} shouldTheComponentsShow={true} />
-    <hr />
-
-    <PersonIterator />
-
-    <hr />
-
-    <StudentClass fruits={fruits} name={name} age={age} shouldTheComponentsShow={toShow} />
-
-
-    </>);
-}
-
-// function App() {
-//   return (
-//     <div>Hello World</div>
-//   );
-// }
 
 export default App;
